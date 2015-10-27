9f099ee80dc409a634f0ada13724d132e918cb70
Maks <samael500@gmail.com>
2015-10-27 20:51:13 +0300

    :white_check_mark: add views tests

---
+++ helena/content/tests/tests_views.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+from django.test import TransactionTestCase
+
+
+class ContentViewTestCase(TransactionTestCase):
+
+    """ Tests for content view check """
+
+    def setUp(self):
+        self.url_index = reverse('content:index')
+        self.url_genres = reverse('content:genres')
+        self.url_gallery = reverse('content:gallery')
+
+    def test_url_resolved(self):
+        """ Check url reverse correct """
+        self.assertEqual(self.url_index, '/')
+        self.assertEqual(self.url_genres, '/genres')
+        self.assertEqual(self.url_gallery, '/gallery')
+
+    def test_index_page(self):
+        """ Get 200 on index page """
+        response = self.client.get(self.url_index)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Сохраните лучшие моменты...', response.content.decode('utf-8'))
+
+    def test_genres_page(self):
+        """ Get 200 on genres page """
+        response = self.client.get(self.url_genres)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '<li><a href="%s" class=\'active\'>Жанры</a></li>' % self.url_genres,
+            response.content.decode('utf-8'))
+
+    def test_gallery_page(self):
+        """ Get 200 on gallery page """
+        response = self.client.get(self.url_gallery)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '<li><a href="%s" class=\'active\'>Галерея</a></li>' % self.url_gallery,
+            response.content.decode('utf-8'))
