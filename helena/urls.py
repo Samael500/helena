be3e2c3519d0a2ad134f8eef939ac22b8b3189f6
Maks <samael500@gmail.com>
2015-10-27 20:43:27 +0300

    :repeat: use url namespace

--- helena/urls.py
+++ helena/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('content.urls')),
+    url(r'^', include('content.urls', namespace='content')),
     # url(r'^blog/', include('blog.urls')),
 ]
