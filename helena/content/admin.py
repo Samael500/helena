a51c1ba818551e93de5a73ca9d0caa9ca9df9aad
Maks <samael500@gmail.com>
2015-10-31 22:10:39 +0300

    :repeat: register gallery in admin

--- helena/content/admin.py
+++ helena/content/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from content.models import Genres
+from content.models import Genres, Galleries
 
 
 class GenresAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class GenresAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'description')
 
 
+class GalleriesAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'title', 'img_url')
+
+
 admin.site.register(Genres, GenresAdmin)
+admin.site.register(Galleries, GalleriesAdmin)
