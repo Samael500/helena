f8136207f9fcb739206908be41f4e0ff3009faa0
Maks <samael500@gmail.com>
2015-11-12 19:39:43 +0300

    :repeat: update feedback form with error css

--- helena/content/forms.py
+++ helena/content/forms.py
@@ -3,20 +3,23 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator
 # from captcha.fields import ReCaptchaField
 
+from helpers.forms import UpdateCssMixin
 
-class FeedBackForm(forms.Form):
+
+class FeedBackForm(forms.Form, UpdateCssMixin):
 
     """ Send email with feedback """
 
-    required_msg = 'Обязательное поле.'
-    email_invalid = 'Введите корректный адрес электронной почты.'
-
     # captcha = ReCaptchaField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._set_errors_css()
 
     name = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Имя'}), required=True, max_length=200)
     email = forms.EmailField(widget=forms.TextInput(
-        attrs={'placeholder': 'Email'}), required=True, max_length=200, error_messages=dict(invalid=email_invalid))
+        attrs={'placeholder': 'Email'}), required=True, max_length=200)
     theme = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Тема'}), required=True, max_length=200)
     message = forms.CharField(widget=forms.Textarea(
